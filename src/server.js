46515fef67c4884d4d6fe50625c41d90a6421e36
Milbonn Yaya <milbonn@levitate.io>
2017-05-02 17:59:54 -0700

    Running express server to serve on heroku

---
+++ src/server.js
@@ -0,0 +1,7 @@
+let app = require('./express');
+
+app.set('port', process.env.PORT || 3000);
+
+var server = app.listen(app.get('port'), function() {
+  console.log('Express server listening on port ' + server.address().port);
+});
